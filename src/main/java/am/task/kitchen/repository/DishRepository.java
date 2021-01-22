9da2cda067424e01515fa1956b904ac9c4cf6f34
ruben.manukyan <ruben.manukyan@iunetworks.am>
2021-01-22 14:47:13 +0400

    added get possible dishes query

--- src/main/java/am/task/kitchen/repository/DishRepository.java
+++ src/main/java/am/task/kitchen/repository/DishRepository.java
@@ -3,7 +3,6 @@ package am.task.kitchen.repository;
 import am.task.kitchen.model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,11 +10,23 @@ import java.util.List;
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM  dish  where id in" +
-            "(    SELECT  dish_id  FROM dish_ingredient " +
-            " where ingridient_id in(1) " +
-            " and (SELECT ingridient.count from  ingridient where id  = ingridient_id )> 0" +
-            "  )")
+    String possible_dishes_query = "SELECT * FROM dish " +
+            "WHERE id IN ( " +
+            "    SELECT dish_id " +
+            "    FROM dish_ingredient di " +
+            "    WHERE (SELECT COUNT(*) " +
+            "           FROM dish_ingredient " +
+            "           WHERE dish_id = di.dish_id) = (SELECT COUNT(*) " +
+            "                                          FROM ingredient " +
+            "                                          WHERE ingredient.count > 0 " +
+            "                                            AND ingredient.id IN " +
+            "                                                (SELECT ingredient_id FROM dish_ingredient WHERE dish_id = di.dish_id) " +
+            "          )" +
+            ")";
+
+
+
+    @Query(nativeQuery = true, value = possible_dishes_query)
     List<Dish> getAllPossibleDishes();
 
 
